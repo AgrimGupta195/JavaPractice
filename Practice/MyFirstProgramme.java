b73a7829f1d0df6c0405cd57e57c90e911188513
Sumit Gupta <71562835+sumitgupta09@users.noreply.github.com>
2023-03-24 10:17:55 +0530

    added java p

---
+++ Practice/MyFirstProgramme.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class MyFirstProgramme {
+
+    public static void main(String[] args) {
+      boolean x=true;
+      if(x)
+          System.out.println("tuv");
+      else
+          System.out.println("xuv");
+    }
+}
